967b7414e22e029e498da0cb7b9e36d9dc0032b2
gabei <gabe1792@gmail.com>
2025-04-29 12:42:24 -0500

    Sets up basic response from server for file upload so the client doesnt get hung up

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 
 const upload = multer({ 
   storage: storage,
-  fileFilter: fileFilterOptions,
+  //fileFilter: fileFilterOptions,
 });
 
 
@@ -26,13 +26,12 @@ app.get('/', async (req, res, next) => {
 
 
 app.post('/reorder', upload.array('file-select', 5), async (req, res, next) => {
-  if(req.file){
-    res.status(200).send({response: 'File received.'});
-  }
-  
   console.log("/reorder hit");
-  console.log(req.file);
-  console.log(req.body);
+
+  if(req.files){
+    res.status(200).send({response: 'File received.'});
+    console.log(req.files);
+  }
 });
 
 
